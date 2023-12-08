b78014425a5fad2a43ba1074c7ae04b025e481ff
najamoe <najamoe@outlook.dk>
2023-12-08 12:02:09 +0100

    changed styling to navbar

--- js/navbar-footer.js
+++ js/navbar-footer.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Event listener for the dropdown button
     document.getElementById("dropdownBtnContact").addEventListener("click", function () {
-        openDropdown();
+        toggleDropdown();
     });
 
     // Event listener to close the dropdown when clicking outside of it
@@ -11,7 +11,6 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-
     // Event listeners for buttons
     document.getElementById('logo-button').addEventListener('click', function () {
         navigateTo('index.html');
@@ -20,10 +19,26 @@ document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('aboutme-button').addEventListener('click', function () {
         navigateTo('aboutmepage.html');
     });
+
     document.getElementById('treatments-button').addEventListener('click', function () {
         navigateTo('treatments.html');
     });
+
     document.getElementById('booking-button').addEventListener('click', function () {
         navigateTo('booking.html');
     });
 });
+
+function toggleDropdown() {
+    var dropdownContent = document.querySelector('.dropdown-content');
+    dropdownContent.style.display = (dropdownContent.style.display === 'block') ? 'none' : 'block';
+}
+
+function closeDropdown() {
+    var dropdownContent = document.querySelector('.dropdown-content');
+    dropdownContent.style.display = 'none';
+}
+
+function navigateTo(page) {
+    window.location.href = page;
+}
